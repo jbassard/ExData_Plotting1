70d24d3150cc1ba9e12a86efb8995f678332943c
jbassard <jbassard@outlook.com>
2017-05-07 15:33:38 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -33,7 +33,7 @@ plot(date_time, as.numeric(as.character(Power$Voltage)), type="l", xlab="datetim
 plot(date_time, Power$Sub_metering_1, type="l", xlab="", ylab="Energy Sub metering")
 lines(date_time, Power$Sub_metering_2, type = "l", col = "red" )
 lines(date_time, Power$Sub_metering_3, type = "l", col = "blue" )
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=3, col=c("black", "red", "blue"), box.lty=0  #box.lty=0 to remove the legend boarder
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2, col=c("black", "red", "blue"), box.lty=0)  #box.lty=0 to remove the legend boarder
 
 ##Making graph 4
 plot(date_time, as.numeric(as.character(Power$Global_reactive_power)), type="l", xlab="datetime", ylab="Global_Rective_Power")
